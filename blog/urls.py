6b58f33d0ead729c2fb370627f982686e2e84444
Pawan Bakle <pawankb2005@gmail.com>
2023-10-16 17:52:06 +0530

    Django Signals

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path,include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 from userPage import views as user_view
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('profile/', user_view.profile, name="profile"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- userPage/apps.py
+++ userPage/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UserpageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userPage'
+
+    def ready(self):
+        import userPage\
+            .signals
