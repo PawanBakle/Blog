c87234f99f2235ceda9fbbaf38b20aeea27f7aa5
Pawan Bakle <pawankb2005@gmail.com>
2024-10-17 12:44:18 +0530

    comment section

--- feed/models.py
+++ feed/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.admin import User
+from django.contrib.auth.models import User
 from userPage.models import Profile
 class Posts(models.Model):
 
@@ -7,5 +7,14 @@ class Posts(models.Model):
     content = models.TextField()
     # date_posted = models.DateField()
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
+class Comments(models.Model):
+    text = models.TextField()
+    u_name = models.ForeignKey(User,on_delete=models.CASCADE)
+    posts = models.ForeignKey(Posts,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.text[:20]
+    
+
