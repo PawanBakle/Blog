9b936a57067710d8555de3c84fbefbd7af52c15d
Pawan Bakle <pawankb2005@gmail.com>
2024-10-23 04:35:58 +0530

    add tags to the post

--- feed/models.py
+++ feed/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from userPage.models import Profile
 class Posts(models.Model):
-
+    choices = (('tech','tech'),('news','news'),('health','health')
+    )
     title = models.CharField(max_length=50)
     content = models.TextField()
     # date_posted = models.DateField()
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    
+    tag = models.CharField(max_length=100,choices=choices)
     def __str__(self):
         return self.title
 class Comments(models.Model):
